6843d6d29035ed5524feeff5eccef58a8f9f8dd0
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2017-05-30 18:45:06 +0100

    [HTN][VNG] Fixed server not broadcasting to clients. Also respects separate rooms.

--- suddendev/events.py
+++ suddendev/events.py
@@ -9,17 +9,19 @@ NAMESPACE = '/chat-session'
 @socketio.on('joined', namespace=NAMESPACE)
 def joined(message):
     """Sent by clients when they enter a room. """
-    fsio.emit('status', {'msg': 'someone has joined!'}, namespace=NAMESPACE)
-
+    room = flask.session.get('room_key')
+    fsio.join_room(room)
+    fsio.emit('status', {'msg': 'someone has joined!'}, namespace=NAMESPACE, room=room)
 
 @socketio.on('text', namespace=NAMESPACE)
 def text(message):
     """Sent by a client when the user entered a new message."""
-    fsio.emit('message', {'msg': 'someone' + ':' + message['msg']}, namespace=NAMESPACE)
-
+    room = flask.session.get('room_key')
+    fsio.emit('message', {'msg': 'someone' + ':' + message['msg']}, namespace=NAMESPACE, room=room)
 
 @socketio.on('left', namespace=NAMESPACE)
 def left(message):
     """Sent by clients when they leave a room."""
-    fsio.emit('status', {'msg': 'somebody' + ' has left the room.'}, namespace=NAMESPACE)
-
+    room = flask.session.get('room_key')
+    fsio.emit('status', {'msg': 'somebody' + ' has left the room.'}, namespace=NAMESPACE, room=room)
+    fsio.leave_room(room)
