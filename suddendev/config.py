c378cb59bbd40cfb83d4449b9859e1d9a241cd94
Nik Gupta <vngupta77@gmail.com>
2017-05-31 16:16:33 +0100

    [NG] Suppress annoying track SQLAlchemy modifications warning by disabling.

--- suddendev/config.py
+++ suddendev/config.py
@@ -8,7 +8,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     DEBUG = False
