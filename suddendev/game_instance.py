54a0c4f1dced9a734a42201793e869d3e6f78682
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2017-05-31 15:02:48 +0100

    [HTN] Fixed json status updates

--- suddendev/game_instance.py
+++ suddendev/game_instance.py
@@ -7,7 +7,85 @@ import flask_socketio as fsio
 import datetime
 
 NAMESPACE = '/game-session'
-
+sample_json="""
+{
+	"players": [{
+		"tag": 0,
+		"healthMax": 100,
+		"health": 100,
+		"ammo": 10,
+		"name": "Bob",
+		"color": {
+			"r": 255,
+			"g": 255,
+			"b": 255
+		},
+		"pos": {
+			"x": 100,
+			"y": 500
+		},
+		"vel": {
+			"x": 100,
+			"y": 500
+		},
+		"size": {
+			"x": 100,
+			"y": 500
+		}
+	}],
+	"enemies": [{
+		"tag": 0,
+		"healthMax": 100,
+		"health": 100,
+		"pos": {
+			"x": 100,
+			"y": 500
+		},
+		"vel": {
+			"x": 100,
+			"y": 500
+		},
+		"size": {
+			"x": 100,
+			"y": 500
+		}
+	}],
+	"powerups": [{
+		"tag": 0,
+		"healthMax": 100,
+		"health": 100,
+		"pos": {
+			"x": 100,
+			"y": 500
+		},
+		"vel": {
+			"x": 100,
+			"y": 500
+		},
+		"size": {
+			"x": 100,
+			"y": 500
+		}
+	}],
+	"core": {
+		"tag": 0,
+		"healthMax": 100,
+		"health": 100,
+		"pos": {
+			"x": 100,
+			"y": 500
+		},
+		"vel": {
+			"x": 100,
+			"y": 500
+		},
+		"size": {
+			"x": 100,
+			"y": 500
+		}
+	}
+}
+"""
 class GameInstance:
     def __init__(self, game_id, app):
         self.app = app
@@ -19,10 +97,10 @@ class GameInstance:
         while True:
             self.game.tick(10)
             #json = encodeState(state)
-            json = {"hello" : "noob"}
+            json = sample_json
             with self.app.app_context():
-                # fsio.emit('status', json, namespace=NAMESPACE, room=self.game_id)
-                fsio.emit('status', json, namespace=NAMESPACE, broadcast=True)
+                fsio.emit('status', json, namespace=NAMESPACE, room=self.game_id)
+                # fsio.emit('status', json, namespace=NAMESPACE, broadcast=True)
 
     # join
     # leave
