4b10482105fe3b1256a68140880f6da783c7a85c
Nik Gupta <vngupta77@gmail.com>
2017-05-31 17:53:21 +0100

    [VNG] Remove artefacts from previous application.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,14 +1,6 @@
 import pytest
-from suddendev.models import ChatRoom
+from suddendev.models import GameController
 
 
-def test_create_chat_room(session):
-
-    room_key = "ASDF"
-
-    chat_room = ChatRoom(room_key=room_key)
-    session.add(chat_room)
-    session.commit()
-
-    assert chat_room.id > 0
-    assert chat_room.room_key == room_key
+def test_create_game(session):
+    assert False
