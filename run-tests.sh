72c3b95b14aea9d19451291731e68dc3716d69e5
Nik Gupta <vngupta77@gmail.com>
2017-06-01 14:35:48 +0100

    [NG] Update travis to use tox command as work around for broken test script.

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+export DATABASE_URL=postgresql:///suddendev_test
+export APP_SETTINGS=suddendev.config.TestingConfig
+export SECRET_KEY=test_key_pls_ignore
+export REDIS_URL=redis://localhost:6379/0
 sh run-redis.sh &
 venv/bin/celery worker -A suddendev.celery --loglevel=info &
 tox
