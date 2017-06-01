4ff1eb00f8e212d280ac858feb4efcc795d97d80
Nik Gupta <vngupta77@gmail.com>
2017-06-01 20:05:57 +0100

    [NG] Fix broken import in model tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,7 @@
 import pytest
-from suddendev.models import GameController
+from suddendev.models import GameSetup
 
 
 def test_create_game(session):
-    pass
+    game_setup = GameSetup('ASDF')
+    assert game_setup.player_count == 1
