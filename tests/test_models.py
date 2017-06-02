f300e88e86994dda0204818a51b153a23d90fe6e
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2017-06-02 10:56:42 +0100

    [HTN] GameSetup test now tests for the right value

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,4 +4,4 @@ from suddendev.models import GameSetup
 
 def test_create_game(session):
     game_setup = GameSetup('ASDF')
-    assert game_setup.player_count == 1
+    assert game_setup.player_count == 0
