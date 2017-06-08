8c7fec20474e5eacbac6ec735b43eb5a5add6f89
Nik Gupta <vngupta77@gmail.com>
2017-06-08 16:44:29 +0100

    [NG][DD][HTN] Fix messaging interface with front-end.

--- suddendev/tasks.py
+++ suddendev/tasks.py
@@ -5,15 +5,15 @@ import json
 import flask_socketio as fsio
 
 @celery.task
-def play_game(game_id, player_names, scripts, namespace):
+def play_game(game_id, player_names, scripts, namespace, room):
     game = GameInstance(game_id, player_names, scripts)
     socketio = fsio.SocketIO(message_queue=Config.REDIS_URL)
 
     for batch, log in game.run():
         if log is not None:
             for e in log['errors']:
-                socketio.emit('message', '[ERROR] ' + e, namespace=namespace)
-            for e in log['stdout']:
-                socketio.emit('message', '[OUT] ' + e, namespace=namespace)
+                socketio.emit('message_error', '[ERROR] ' + e, room=room, namespace=namespace)
+            #for e in log['stdout']:
+            #    socketio.emit('message_print', '[OUT] ' + e, room=room, namespace=namespace)
 
-        socketio.emit('result', '{\"result\": [ ' + ','.join(batch) + ']}', room=game_id, namespace=namespace)
+        socketio.emit('result', '{\"result\": [ ' + ','.join(batch) + ']}', room=room, namespace=namespace)
